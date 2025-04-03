79b6282e80fd7d76fc0547be277d0b5603f852a1
Mücahit <71331160+mucahitkalabalik@users.noreply.github.com>
2025-04-03 13:49:06 +0300

    Update main.js

--- main.js
+++ main.js
@@ -73,7 +73,7 @@ const speak = () => {
   row.style.backgroundSize = "100% 100%";
 
   const speakText = new SpeechSynthesisUtterance(
-    "Sen niye bu kadar boş yapıyorsun" + nameInput.value + textInput.value
+    "Hoşgeldin" + nameInput.value + textInput.value
   );
 
   speakText.onend = () => {
